790140addfce5006969efbfab777dfa81876e308
Aalekh Sharma <shaalekh@gmail.com>
2025-12-29 23:48:07 +0530

    All Services are created and led control updated

--- led_control/led_control_server.py
+++ led_control/led_control_server.py
@@ -8,6 +8,7 @@ class LedControlClass(Node):
         self.ledstate=False
         self.ledsrv=self.create_service(SetBool, 'led_control', self.led_control_callback)
         #tenary operator is used in logging
+        self.get_logger().info("Led Control Server is active and Running.")
         self.get_logger().warn("Current Led state: ON" if self.ledstate==True else "Current Led state: OFF")
         
     def led_control_callback(self, request, response):
@@ -15,18 +16,17 @@ class LedControlClass(Node):
         b = self.ledstate
         c = not a and b or a and not b
         if c is True:
+            self.ledstate = not self.ledstate
             self.state_trigger()
             response.success = True
             response.message = 'LED turned OFF' if a is False else 'LED turned ON'
         else:
             response.success = False
             response.message = 'LED is already ON' if a is True else 'LED is already OFF'
-        self.get_logger().info(f"Request: {request.data}  Response: {response.success}")
+        self.get_logger().info(f'Request: {"Turn ON" if request.data else "Turn OFF"}  Response: {response.message}')
         
         return response
     
-    def state_trigger(self):
-        self.ledstate = not self.ledstate
 
     
 def main() -> None:
---
+++ led_control/safety_check_service.py
@@ -0,0 +1,33 @@
+import rclpy
+from rclpy.node import Node
+from std_srvs.srv import Trigger
+
+class SafetyCheckServerClass(Node):
+    def __init__(self):
+        super().__init__('safety_check_server')
+        self.emergency_stop_status=True
+        self.create_service(Trigger, 'safety_check', self.safety_check_callback)
+        self.get_logger().info("Safety Check Server is active and running")
+
+    def safety_check_callback(self, request , response):
+        response.success = False if self.emergency_stop_status else True
+        response.message = "Emergency Stop Activated" if self.emergency_stop_status else "Safety Check OK"
+
+        self.get_logger().info(f"Safety Check Response: {response.message}")
+
+        return response
+
+def main() -> None:
+    try:
+        rclpy.init()
+        node=SafetyCheckServerClass()
+        rclpy.spin(node)
+        node.destroy_node()
+        rclpy.shutdown()
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        node.get_logger().error(e)
+
+if __name__=='__main__':
+    main()
